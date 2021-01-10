81244afba135b40521a50f575f19fb714828ce53
Elias Seeve <elias.seeve@gmail.com>
2021-01-10 10:49:20 +0200

    Testing healthcheck

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('2')
+})
+
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
